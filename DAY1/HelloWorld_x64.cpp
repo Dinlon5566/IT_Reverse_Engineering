c223478555a9c190a712a932497cbf4a62070ea0
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2022-09-08 19:20:19 +0800

    day_1
    update day one file

---
+++ DAY1/HelloWorld_x64.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <ctype.h>
+int main()
+{
+	MessageBox(0i64,L"Hello World",L"this is title.",MB_OK);
+	return 0;
+}
---
+++ DAY1/try_hackme_day1.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	//Hidden
+	string password = "?????????";
+	printf("Input the password :\n");
+
+	getline(cin,input);
+	if (input == password) {
+		printf("Correct");
+	}
+	else {
+		printf("Wrong");
+	}
+}
