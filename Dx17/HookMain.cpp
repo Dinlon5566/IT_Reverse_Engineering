bfa677518676ecc4aec74bb59f3d216379d471d1
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2022-09-22 17:06:21 +0800

    update Dx17

---
+++ Dx17/HookMain.cpp
@@ -0,0 +1,37 @@
+﻿#include "stdio.h"
+#include "conio.h"
+#include "windows.h"
+
+#define	DEF_DLL_NAME		"KeyHook.dll"
+#define	DEF_HOOKSTART		"HookStart"
+#define	DEF_HOOKSTOP		"HookStop"
+
+typedef void (*PFN_HOOKSTART)();
+typedef void (*PFN_HOOKSTOP)();
+
+void main()
+{
+	HMODULE			hDll = NULL;
+	PFN_HOOKSTART	HookStart = NULL;
+	PFN_HOOKSTOP	HookStop = NULL;
+	char			ch = 0;
+
+	hDll = LoadLibraryA(DEF_DLL_NAME);
+	if (hDll == NULL)
+	{
+		printf("LoadLibrary(%s) failed!!! [%d]", DEF_DLL_NAME, GetLastError());
+		return;
+	}
+
+	HookStart = (PFN_HOOKSTART)GetProcAddress(hDll, DEF_HOOKSTART);
+	HookStop = (PFN_HOOKSTOP)GetProcAddress(hDll, DEF_HOOKSTOP);
+
+	HookStart();
+
+	printf("press 'q' to quit!\n");
+	while (_getch() != 'q');
+
+	HookStop();
+
+	FreeLibrary(hDll);
+}
---
+++ Dx17/readme.md
@@ -0,0 +1,3 @@
+如果你要執行，請確認你有安裝 Microsoft Visual C++可轉散發套件(x64)。
+另外，執行時請到 cmd.exe 來執行。
+這個用 Visual Studio 2019 建置。
