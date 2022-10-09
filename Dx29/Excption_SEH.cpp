1994390d4d855e98480f9de642db9aade4eec61c
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2022-10-09 22:21:36 +0800

    dx29

---
+++ Dx29/Excption_SEH.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "windows.h"
+
+void debugerFound() {
+	MessageBox(0, L"Debuger Found", L"Excption_SEH", 0);
+}
+int main()
+{
+	printf("Start");
+	__asm {
+		push handler
+		push DWORD PTR fs : [0]
+		mov DWORD PTR fs:[0], esp
+
+		int 0x03
+
+		call debugerFound
+		mov eax,0xFFFFFFFF
+
+		jmp eax
+
+	handler:
+		mov eax, dword ptr ss : [esp + 0xc]
+		mov ebx, normal_code
+		mov dword ptr ds : [eax + 0xb8] , ebx
+		xor eax, eax
+		retn
+
+	normal_code :
+		pop dword ptr fs : [0]
+		add esp, 4
+
+	}
+	MessageBox(0, L"Save", L"Excption_SEH", 0);
+}
