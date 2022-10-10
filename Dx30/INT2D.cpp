152142928c2332e9515c99d660a851d19a10f18d
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2022-10-10 14:49:33 +0800

    dx30

---
+++ Dx30/INT2D.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <Windows.h>
+void foundDebugger() {
+    MessageBox(0, L"Found Debugger", L"TF", 0);
+}
+int main()
+{
+    __asm {
+        // 載入 SEH
+        push handler
+        push DWORD ptr fs : [0]
+        mov DWORD ptr fs : [0] , esp
+        xor eax,eax
+        // 觸發例外
+        int 0x2d
+
+        // 如果執行這行會使 EAX 變成 1
+        mov EAX, 0x1
+ 
+        // 確認 EAX 的值
+        Continue:
+        cmp EAX,0x0
+        je normal_code
+
+        // 發現 Debuger
+        call foundDebugger
+        mov eax,0xFFFFFFFF
+        jmp eax 
+        
+     handler :
+        // 處裡例外
+        mov eax, dword ptr ss : [esp + 0xc]
+        mov dword ptr ds : [eax + 0xb8] , offset Continue
+        xor eax,eax  // 設定 EAX=0
+        retn
+
+    normal_code :
+        // 移除 SEH
+        pop dword ptr fs : [0]
+        add esp, 4
+       
+    }
+    printf("SAVE\n");
+    return 0;
+}
+
---
+++ Dx30/TrapFlag.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+void foundDebugger() {
+    MessageBox(0, L"Found Debugger", L"TF", 0);
+}
+int main()
+{
+    printf("Start\n");
+    __asm {
+        // 設定 SEH
+        push handler
+        push DWORD ptr fs : [0]
+        mov DWORD ptr fs : [0] , esp
+
+        // 由於無法直接設置 TF，所以把 EFLAGS 抓出來後把第 9 bit 設為 1 再存進 EFLAGS
+        pushfd
+        // 0x100=100000000b
+        or dword ptr ss : [esp] , 0x100
+        popfd
+
+        // 這邊會觸發 TF 引發例外拋出。
+        nop
+
+        // 若是例外被處裡或是使用單步模式，就會跑來這邊。
+        call foundDebugger
+        mov eax, 0xFFFFFFFF
+        jmp eax
+
+        // 例外處裡器
+    handler :
+        mov eax, dword ptr ss : [esp + 0xc]
+        mov dword ptr ds : [eax + 0xb8] , offset normal_code
+        xor eax, eax
+        retn
+
+        // 移除 SEH
+        normal_code :
+        pop dword ptr fs : [0]
+        add esp, 4
+
+    }
+    MessageBox(0, L"SAVE", L"TF", 0);
+}
